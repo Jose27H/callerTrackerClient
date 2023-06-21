6a77113439194382abfae70459718724c493a05e
Jose <josehilario27@hotmail.com>
2023-06-21 11:40:02 -0400

    removed the https

--- src/components/patientform/PhoneCheckForm.jsx
+++ src/components/patientform/PhoneCheckForm.jsx
@@ -9,7 +9,7 @@ const Form = () => {
     setNumber(inputValue);
 
     // Perform the POST/fetch API call
-    fetch("https://callertrackerserver.up.railway.app/api/formnumber", {
+    fetch("callertrackerserver.up.railway.app/api/formnumber", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
