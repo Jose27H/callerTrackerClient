c4319b8f2436d5d2cb61a801c803fc4a866a21f1
Jose <josehilario27@hotmail.com>
2023-06-22 01:37:20 -0400

    changed some names

--- src/pages/4.golf/golf.jsx
+++ src/pages/4.golf/golf.jsx
@@ -3,10 +3,13 @@ import GLogin from "../../components/golfiles/golfLogin.jsx";
 
 const Golf = () => {
   return (
-    <div>
-      <h1>Login To Enter Scores</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-green-500">
+      <h1 className="text-4xl text-white font-bold mb-8">
+        Login To Enter Scores
+      </h1>
       <GLogin />
     </div>
   );
 };
+
 export default Golf;
