b9303d6c55238face42af30987a1449ef291e46e
Jose <josehilario27@hotmail.com>
2023-06-22 09:45:14 -0400

    changed golf page to autoredirect if already logged in

--- src/pages/4.golf/golf.jsx
+++ src/pages/4.golf/golf.jsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GLogin from "../../components/golfiles/golfLogin.jsx";
 
 const Golf = () => {
+  useEffect(() => {
+    // Check if there is a value in sessionStorage
+    const golferNumber = sessionStorage.getItem("golferNumber");
+    if (golferNumber) {
+      // Redirect to /Golf/Gprofile
+      window.location.href = "/Golf/Gprofile";
+    }
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-green-500">
       <h1 className="text-4xl text-white font-bold mb-8">
