283384ee560f69a00dbf0e8a65a01e1a4fb4e3e2
Jose <josehilario27@hotmail.com>
2023-06-26 11:06:50 -0400

    added a sate field for the  patient form

--- src/components/patientform/patientForm.jsx
+++ src/components/patientform/patientForm.jsx
@@ -44,6 +44,7 @@ const Form = () => {
           month: "",
           day: "",
           year: "",
+          state: "",
           message: "",
         });
 
@@ -70,6 +71,61 @@ const Form = () => {
     "October",
     "November",
     "December",
+  ];
+
+  const states = [
+    { code: "", name: "Select state" },
+    { code: "AL", name: "Alabama" },
+    { code: "AK", name: "Alaska" },
+    { code: "AZ", name: "Arizona" },
+    { code: "AR", name: "Arkansas" },
+    { code: "CA", name: "California" },
+    { code: "CO", name: "Colorado" },
+    { code: "CT", name: "Connecticut" },
+    { code: "DE", name: "Delaware" },
+    { code: "FL", name: "Florida" },
+    { code: "GA", name: "Georgia" },
+    { code: "HI", name: "Hawaii" },
+    { code: "ID", name: "Idaho" },
+    { code: "IL", name: "Illinois" },
+    { code: "IN", name: "Indiana" },
+    { code: "IA", name: "Iowa" },
+    { code: "KS", name: "Kansas" },
+    { code: "KY", name: "Kentucky" },
+    { code: "LA", name: "Louisiana" },
+    { code: "ME", name: "Maine" },
+    { code: "MD", name: "Maryland" },
+    { code: "MA", name: "Massachusetts" },
+    { code: "MI", name: "Michigan" },
+    { code: "MN", name: "Minnesota" },
+    { code: "MS", name: "Mississippi" },
+    { code: "MO", name: "Missouri" },
+    { code: "MT", name: "Montana" },
+    { code: "NE", name: "Nebraska" },
+    { code: "NV", name: "Nevada" },
+    { code: "NH", name: "New Hampshire" },
+    { code: "NJ", name: "New Jersey" },
+    { code: "NM", name: "New Mexico" },
+    { code: "NY", name: "New York" },
+    { code: "NC", name: "North Carolina" },
+    { code: "ND", name: "North Dakota" },
+    { code: "OH", name: "Ohio" },
+    { code: "OK", name: "Oklahoma" },
+    { code: "OR", name: "Oregon" },
+    { code: "PA", name: "Pennsylvania" },
+    { code: "RI", name: "Rhode Island" },
+    { code: "SC", name: "South Carolina" },
+    { code: "SD", name: "South Dakota" },
+    { code: "TN", name: "Tennessee" },
+    { code: "TX", name: "Texas" },
+    { code: "UT", name: "Utah" },
+    { code: "VT", name: "Vermont" },
+    { code: "VA", name: "Virginia" },
+    { code: "WA", name: "Washington" },
+    { code: "WV", name: "West Virginia" },
+    { code: "WI", name: "Wisconsin" },
+    { code: "WY", name: "Wyoming" },
+    { code: "Other", name: "Other" },
   ];
 
   const days = Array.from({ length: 31 }, (_, index) => index + 1);
@@ -197,6 +253,29 @@ const Form = () => {
           </select>
         </div>
       </div>
+
+      <div className="mb-4">
+        <label
+          htmlFor="state"
+          className="block text-gray-700 font-semibold mb-1"
+        >
+          State:
+        </label>
+        <select
+          id="state"
+          name="state"
+          value={formData.state}
+          onChange={handleInputChange}
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+        >
+          {states.map((state) => (
+            <option key={state.code} value={state.code}>
+              {state.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <div className="mb-4">
         <label
           htmlFor="message"
