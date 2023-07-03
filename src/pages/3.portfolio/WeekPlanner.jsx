e7c89e86c07d46b05bd949f78ace79edb3aec437
Jose <josehilario27@hotmail.com>
2023-07-03 13:22:39 -0400

    made it nicer on the eyes

--- src/pages/3.portfolio/WeekPlanner.jsx
+++ src/pages/3.portfolio/WeekPlanner.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const WeekPlanner = () => {
   const handleButtonClick = () => {
-    // Logic or actions to perform when the button is clicked
     console.log("Button clicked!");
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white shadow-md rounded-lg p-8">
+    <div className="h-screen flex flex-col items-center justify-center bg-white shadow-md rounded-lg p-8">
       <a
         href="https://calendar.app.google/Cf2w2pQGRKooYUww5"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-8"
@@ -15,7 +14,7 @@ const WeekPlanner = () => {
       >
         Hacer Cita
       </a>
-      <div className="w-full">
+      <div className="w-full h-96">
         <iframe
           src="https://calendar.google.com/calendar/embed?src=hyperbariccarecenter%40gmail.com&ctz=America%2FNew_York"
           title="Google Calendar"
