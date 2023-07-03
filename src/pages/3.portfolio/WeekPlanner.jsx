9365c5049fa0c9cbecfda5e5d740c098fb75d507
Jose <josehilario27@hotmail.com>
2023-07-03 12:27:55 -0400

    added scheduling feature and also mobile sizing for phone use

--- src/pages/3.portfolio/WeekPlanner.jsx
+++ src/pages/3.portfolio/WeekPlanner.jsx
@@ -7,21 +7,23 @@ const WeekPlanner = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div
+      className="h-screen flex flex-col items-center justify-center"
+      style={{ padding: "20px" }}
+    >
       <a
         href="https://calendar.app.google/Cf2w2pQGRKooYUww5"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleButtonClick}
+        style={{ marginBottom: "20px" }}
       >
         Hacer Cita
       </a>
-      <div className="mt-4">
+      <div style={{ marginTop: "20px", width: "100%" }}>
         <iframe
           src="https://calendar.google.com/calendar/embed?src=hyperbariccarecenter%40gmail.com&ctz=America%2FNew_York"
           title="Google Calendar"
-          style={{ border: 0 }}
-          width="800"
-          height="600"
+          style={{ border: 0, width: "100%", height: "600px" }}
           frameBorder="0"
           scrolling="no"
         ></iframe>
