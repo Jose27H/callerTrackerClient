211b23cf54acad9f7b29b7b047839d6d7dc363b5
Jose <josehilario27@hotmail.com>
2023-07-03 12:50:26 -0400

    added tailwind css to make the components work better and not go up into header

--- src/pages/3.portfolio/WeekPlanner.jsx
+++ src/pages/3.portfolio/WeekPlanner.jsx
@@ -7,23 +7,19 @@ const WeekPlanner = () => {
   };
 
   return (
-    <div
-      className="h-screen flex flex-col items-center justify-center"
-      style={{ padding: "20px" }}
-    >
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white shadow-md rounded-lg p-8">
       <a
         href="https://calendar.app.google/Cf2w2pQGRKooYUww5"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-8"
         onClick={handleButtonClick}
-        style={{ marginBottom: "20px" }}
       >
         Hacer Cita
       </a>
-      <div style={{ marginTop: "20px", width: "100%" }}>
+      <div className="w-full">
         <iframe
           src="https://calendar.google.com/calendar/embed?src=hyperbariccarecenter%40gmail.com&ctz=America%2FNew_York"
           title="Google Calendar"
-          style={{ border: 0, width: "100%", height: "600px" }}
+          className="border-0 w-full h-96"
           frameBorder="0"
           scrolling="no"
         ></iframe>
