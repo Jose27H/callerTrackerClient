b2535a9a6e4475319545b497c1ce99d136a9ddaf
Jose <josehilario27@hotmail.com>
2023-07-04 09:39:56 -0400

    made the calendar a bit better looking

--- src/pages/3.portfolio/WeekPlanner.jsx
+++ src/pages/3.portfolio/WeekPlanner.jsx
@@ -16,13 +16,6 @@ const WeekPlanner = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <a
-        href="https://calendar.app.google/Cf2w2pQGRKooYUww5"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-8"
-        onClick={handleButtonClick}
-      >
-        Hacer Cita
-      </a>
       <div className="w-screen h-4/5  rounded-lg overflow-hidden px-8">
         <iframe
           src="https://calendar.google.com/calendar/embed?src=hyperbariccarecenter%40gmail.com&ctz=America%2FNew_York"
